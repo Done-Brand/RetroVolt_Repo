a4183ca9e16cd91a2be90688e4be68e8ab4e00dd
Done-Brand <donedev8@gmail.com>
2024-02-12 21:52:44 +0200

    Database Creation
    Connection String created as well as EntityFrameworkCore configured.

---
+++ RetroVolt_Solutions/RetroVolt/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RetroVolt.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Any options configured here would be passed on to the base class DbContext.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
---
+++ RetroVolt_Solutions/RetroVolt/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetroVolt.Models
+{
+    public class Category 
+    {
+        //columns in table (properties)
+        //Use data annotation to allocate Key
+
+        [Key]
+        public int Id { get; set; } //PK
+        [Required]
+        public string Name { get; set; } //will have a not null setting -> required
+        public int DisplayOrder { get; set; }
+
+    }
+}
