54ff7c5063c4e27be9076b830b3e9fdcbc285776
Done-Brand <donedev8@gmail.com>
2024-02-28 09:50:35 +0200

    EntityFramework_Configurations
    Configurations done for razor pages. Added the NuGet packages. Created dbContext class with contructor. Passed connection string and added service in program.cs container.

---
+++ RetroVolt_Solutions/RetroVoltRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+
+namespace RetroVoltRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+       
+
+       
+    }
+}
